37473b691975e3b478ce7890ac258890c3a4bc36
TryToCompile <adam1999@tut.by>
2020-03-22 21:40:23 +0300

    feat: add last

--- src/what-season.js
+++ src/what-season.js
@@ -1,8 +1,10 @@
-function getSeason(date) {
-    month = date.getMonth();
-    console.log(month);
-    
-  return month;
+module.exports = function getSeason(date) {
+  if (typeof date == "undefined") return "Unable to determine the time of year!";
+  if (date.hasOwnProperty("getMonth")) throw new Error();
+  let month = date.getMonth();
+  if(month == 0 || month == 1 || month == 11) return "winter";
+  if(2 <= month && month <= 4) return "spring";
+  if(5 <= month && month <= 7) return "summer";
+  if(8 <= month && month <= 10) return "autumn";
+  return false;
 };
-
-getSeason(new Date(488, 1, 4, 15, 48, 52, 400));
